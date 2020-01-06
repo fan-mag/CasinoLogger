b39c4909d7d1af0e599300a681d58dc7eaae6293
fan-mag <fan-mag@mail.ru>
2020-01-06 19:23:57 +0300

    1.0.1 added CasinoLib integration

--- src/main/java/model/Event.kt
+++ src/main/java/model/Event.kt
@@ -2,4 +2,5 @@ package model
 
 import java.util.*
 
+@Deprecated(message = "Deprecated. Use model of CasinoLib")
 class Event(var id: Int? = null, var service: String? = null, var message: String, var date: Date? = null)
